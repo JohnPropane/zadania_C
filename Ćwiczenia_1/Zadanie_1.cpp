421254fa26c78e063a0b8031721027de47c9b032
JohnPropane <147393823+JohnPropane@users.noreply.github.com>
2024-02-24 22:21:01 +0100

    Add files via upload

---
+++ Ćwiczenia_1/Zadanie_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    printf("Podaj imie: ");
+    scanf_s("%s", name, (unsigned)_countof(name));
+    printf("Hello %s", name);
+
+    return 0;
+}
+
---
+++ Ćwiczenia_1/Zadanie_5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+
+
+int main()
+{
+	int numer = 10;
+	int suma = 0;
+	if (numer <= 2) {
+		printf("0");
+	}
+	else {
+		for (int i = 0; i < numer; i++) {
+			if (i % 3 == 0 or i % 5 == 0) {
+				printf("%d\n", i);
+				suma += i;
+			}
+		}
+	}
+	printf("%d", suma);
+	return 0;
+}
+
