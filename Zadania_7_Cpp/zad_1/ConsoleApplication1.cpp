fa95f1b0b1645df75861ac66c95d23ae2e00ba08
Hubert Muszalski <147393823+JohnPropane@users.noreply.github.com>
2024-05-13 23:44:57 +0200

    Add files via upload

---
+++ Zadania_7_Cpp/zad_1/ConsoleApplication1.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+int str_to_int(std::string str) {
+    std::string numsStr[10] = { "zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    for (int i = 0; i < 10; i++) {
+        if (str == numsStr[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void reverse_str(std::string& str) {
+    std::reverse(str.begin(), str.end());
+}
+
+int find_first_number(std::string str) {
+    std::string numsStr[10] = { "zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    for (int i = 1; i < str.length() + 1; i++) {
+        std::string newStr = str.substr(0, i);
+        if (isdigit(newStr[i - 1])) {
+            return newStr[i - 1] - '0';
+        }
+        for (int j = 0; j < 10; j++) {
+            if (newStr.find(numsStr[j]) != std::string::npos) {
+                return str_to_int(numsStr[j]);
+            }
+        }
+    }
+    return -1;
+}
+
+int find_last_number(std::string str) {
+    std::string numsStr[10] = { "orez", "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin" };
+    reverse_str(str);
+    for (int i = 1; i < str.length() + 1; i++) {
+        std::string newStr = str.substr(0, i);
+        if (isdigit(newStr[i - 1])) {
+            return newStr[i - 1] - '0';
+        }
+        for (int j = 0; j < 10; j++) {
+            if (newStr.find(numsStr[j]) != std::string::npos) {
+                std::string check = numsStr[j];
+                reverse_str(check);
+                return str_to_int(check);
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    std::ifstream fptr("input.txt");
+    if (!fptr) {
+        std::cout << "Nie mozna odczytac pliku";
+        return 1;
+    }
+    
+    std::string myString;
+    int counter = 1;
+    int sum_numbers = 0;
+    int firstNumber = 0;
+    int lastNumber = 0;
+
+    while (std::getline(fptr, myString)) {
+        std::string str_number;
+        firstNumber = find_first_number(myString);
+        lastNumber = find_last_number(myString);
+        str_number = std::to_string(firstNumber) + std::to_string(lastNumber);
+        sum_numbers += std::stoi(str_number);
+        std::cout << counter << ". " << firstNumber << lastNumber << "\n";
+        counter++;
+    }
+
+    std::cout << "Sum of numbers: " << sum_numbers;
+    fptr.close();
+
+    return 0;
+}
---
+++ Zadania_7_Cpp/zad_2/ConsoleApplication1.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+int main()
+{
+    //Wczytanie pliku
+    std::ifstream fptr("input.txt");
+    if (!fptr) {
+        std::cout << "Nie mozna odczytac pliku";
+        return 1;
+    }
+
+    std::string myString;
+
+    int games_power = 0;
+    int data_on = 0;
+    int game_num = 0;
+
+    //Iteracja po każdej linii z pliku
+    while (std::getline(fptr, myString)) {
+        game_num++;
+
+        //Ilość kości elfa
+        int maxRed = 12;
+        int maxBlue = 14;
+        int maxGreen = 13;
+
+        //Minimalna ilość kości potrzebna na gre
+        int currRed = 0;
+        int currBlue = 0;
+        int currGreen = 0;
+
+        //Iteracja po każdym znaku w linii
+        for (int i = 0; i < myString.length(); i++) {
+
+            //Sprawdzenie czy jesteśmy już po liczbie informującej o numerze gry
+            if (myString[i] == ':') {
+                data_on = 1;
+            }
+
+            //Szukanie liczby
+            std::string number;
+            if (data_on == 1) {
+                if (isdigit(myString[i])) {
+                    while (isdigit(myString[i])) {
+                        number += myString[i];
+                        i++;
+                    }
+                }
+
+                //Sprawdzenie jaki kolor jest dla danej liczby
+                if (myString[i + 1] == 'r' && myString[i + 2] == 'e' && myString[i + 3] == 'd') {
+                    int num = std::stoi(number);
+                    if (currRed < num) currRed = num;
+                }
+                else if (myString[i + 1] == 'b' && myString[i + 2] == 'l' && myString[i + 3] == 'u') {
+                    int num = std::stoi(number);
+                    if (currBlue < num) currBlue = num;
+                }
+                else if (myString[i + 1] == 'g' && myString[i + 2] == 'r' && myString[i + 3] == 'e') {
+                    int num = std::stoi(number);
+                    if (currGreen < num) currGreen = num;
+                }
+            }
+        }
+
+        //Obliczanie mocy
+        games_power += currRed * currBlue * currGreen;
+        data_on = 0;
+        if (maxRed >= currRed && maxBlue >= currBlue && maxGreen >= currGreen) {
+            std::cout << "Gra jest mozliwa do zagrania przez elfa\n";
+        }
+        else {
+            std::cout << "Gra jest niemozliwa do zagrania przez elfa\n";
+        }
+        std::cout << "W grze nr." << game_num << " potrzebne jest " << currRed << " czerwona, " << currBlue << " niebieska, " << currGreen << " zielona kostkek, aby gra byla mozliwa\n";
+        std::cout << "-------------------------------------------------------------------------\n";
+    }
+    std::cout << "Moc: " << games_power;
+    return 0;
+}
